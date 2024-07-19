49e394bcb07e04c0dbc041b197918e96872813f2
ArioVeisa <classlearning04@gmail.com>
2024-07-19 03:34:07 +0000

    done

--- formContacs.php
+++ formContacs.php
@@ -81,7 +81,7 @@
         <div id="liveToast" class="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
                 <div class="toast-body">
-                    Are you sure you want to send this message?
+                    Apakah data sudah benar?
                 </div>
                 <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
